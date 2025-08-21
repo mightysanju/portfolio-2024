cae06c36d12cf6dc8e4382f6d21ba91d368140d7
mightysanju <kumarsanju123456789@gmail.com>
2025-08-20 19:08:59 -0500

    Updated Testimonials

--- src/components/TestimonialScroll.tsx
+++ src/components/TestimonialScroll.tsx
@@ -6,25 +6,39 @@ const TestimonialScroll = () => {
 
   const testimonials = [
     {
+      text: '"Sanju provided the TheyPay Carrier Management Team (TCMT) a tailored application "Defect Disputes Override" tool or "DDO" that reduced associate research workload for dispositioning appointment defects carrier incurred working with Amazon.',
+      author: 'Jake Porter',
+      position: 'Senior Supply Chain Manager, Amazon',
+      image:
+        'https://media.licdn.com/dms/image/v2/D5603AQEgbMCz8YxCGQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1706211347116?e=1758758400&v=beta&t=ukCwXKsLtMiuNwAHG0mZpixama_JF0wUmBwlF_vo50w',
+    },
+    {
+      text: "Sanju demonstrated a knowledgeable and professional approach to every task. He quickly grasped the project's nuances and offered insightful solutions to every technical challenge.",
+      author: 'Jaime Martinez',
+      position: 'Manager, General Dynamics Land Systems',
+      image:
+        'https://media.licdn.com/dms/image/v2/D5603AQGgN7ybGzbAwA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726678183711?e=1758758400&v=beta&t=kEeb-mnqJBOH8Z07BfXVSO_1Q_oHNjaJv3h1SOTNBtI',
+    },
+    {
       text: 'Sanju is one of the team members on the floor who always strive towards process excellence.',
       author: 'Deepak Sharma',
       position: 'Operations Manager, Amazon',
       image:
-        'https://media.licdn.com/dms/image/v2/D5603AQERROWVAu8dow/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1670845258471?e=1736985600&v=beta&t=8vwbeBLWl9dmRYRvSXrg5qguF85YoJu8ktUlBs_2Gb8',
+        'https://media.licdn.com/dms/image/v2/D5603AQERROWVAu8dow/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1670845258471?e=1758758400&v=beta&t=ErdqPpc_32QCS0CyJSgf5u_bD25zNBCIq6j-GBy8PS4',
     },
     {
       text: 'He is highly innovative and would love to explore above and beyond his domain.',
       author: 'D V L Sravya',
       position: 'BIE, Amazon',
       image:
-        'https://media.licdn.com/dms/image/v2/C4D03AQHAZWXY64qv1w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1611839569487?e=1736985600&v=beta&t=3go2wONAcYvHx6RsjgYk2Kn_9RxrIXF6Xexi8vSmq8k',
+        'https://media.licdn.com/dms/image/v2/C4D03AQHAZWXY64qv1w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1611839569487?e=1758758400&v=beta&t=IeuMibPc6oDIvOWmoDGhYGan3OO09jsC62JvJarwDHk',
     },
     {
       text: "Sanju's Python programming skills are top-notch, and they consistently deliver efficient and well-structured code.",
       author: 'Hemanth Pittu',
       position: 'Manager, Amazon',
       image:
-        'https://media.licdn.com/dms/image/v2/C5603AQFMJnvXtFaiKw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1607003313161?e=1736985600&v=beta&t=mzl1mbe7P-LCpo71fViLHbXFvPFfidDwHE6wNrswOZ8',
+        'https://media.licdn.com/dms/image/v2/D5603AQGuajNYh2Cl7A/profile-displayphoto-shrink_800_800/B56ZbKtd57GsAc-/0/1747157639537?e=1758758400&v=beta&t=mheENDSn7tOImA2ULTuQwiea2We6kZF2GeHsVtChoA8',
     },
   ];
 
